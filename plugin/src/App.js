155a9a1dc4dd6d9b64dc770caf7a578f34431aa0
Lucas García <lucas@codeccoop.org>
2022-07-07 12:50:30 +0200

    Update plugin package.json; Rename app to consumer; Update README.md;

--- plugin/src/App.js
+++ plugin/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import SomofficeShell from "./main.js";
+import SomofficeShell from "./index.js";
 
 function App() {
   return (
